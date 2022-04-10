bb027a68313243ffdc29ff5cb1fb8aae6d176816
cvantettinavar@gmail.com <cvantettinavar@gmail.com>
2022-04-10 15:14:36 +0530

    4th

---
+++ SimpleP/src/test/java/Profiling/Profile1Test.java
@@ -0,0 +1,29 @@
+package Profiling;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Profile1Test 
+{
+	@Test(groups="SmokeTest")
+	public void hiTest()
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("https://www.google.co.in/");
+		driver.close();
+	}
+	@Test(groups="RegressionTest")
+	public void hiiTest()
+	{
+		System.out.println("hi regression");
+	}
+
+}
